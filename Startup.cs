f54404e8c02b0bc99c217917730a29719735f07f
chenp-fjnu <chp0524@gmail.com>
2016-07-30 14:12:56 +0800

    setup web host

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+namespace WebApiFrame
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context=>{
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
